be1837d4092275bce2b61f38c3ce02d91c1d7762
ethanpikedev <ethanpikedev@gmail.com>
2022-03-27 17:53:06 +0200

    changed admin dashboard

--- new.sh
+++ new.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pass=`sudo cat /var/lib/jenkins/secrets/initialAdminPassword` && echo 'jenkins.model.Jenkins.instance.securityRealm.createAccount("user1", "password123")' | sudo java -jar jenkins-cli.jar -auth admin:$pass -s http://localhost:8080/ groovy =
+pass=`sudo cat /var/lib/jenkins/secrets/initialAdminPassword` && echo 'jenkins.model.Jenkins.instance.securityRealm.createAccount("orange", "F69L8RvtBUtB7j8s")' | sudo java -jar jenkins-cli.jar -auth admin:$pass -s http://localhost:8080/ groovy =
